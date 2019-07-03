14b009e0337b17a04309673ef7647246b14d1cca
Kenneth Choi <kennethichoi@gmail.com>
2019-07-02 20:39:50 -0700

    Complete custom redirect via AuthRoute

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 
 import GreetingContainer from './NavBar/greeting_container';
 import LoginFormContainer from './SessionForm/login_form_container';
@@ -13,8 +14,8 @@ const App = () => {
         <h1>airbnb...</h1>
         <GreetingContainer />      
         <br/>
-        <Route path='/login' component={LoginFormContainer}/>
-        <Route path='/signup' component={SignupFormContainer}/>
+        <AuthRoute path='/login' component={LoginFormContainer}/>
+        <AuthRoute path='/signup' component={SignupFormContainer}/>
       </header>
     </div>
   );
---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Auth = ({component: Component, path, loggedIn, exact}) => (
+  <Route path={path} exact={exact} render={(props) => (
+    !loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to='/' />
+    )
+  )} />
+);
+
+const mapStateToProps = state => {
+  return {loggedIn: Boolean(state.session.id)}
+}
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
