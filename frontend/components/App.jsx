349b35c443e7b00fb4efd2fb9701ad4a8a9320f8
Kenneth Choi <kennethichoi@gmail.com>
2019-07-05 16:26:08 -0700

    Refactor App.jsx for Route conditional rendering

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
 
 import Header from './navbar/header';
 import ListingIndexContainer from './listings/listing_index_container';
+import Landing from './landing/landing';
 
 
 const App = () => {
   return (
     <main className="main__container">
-      <div className="header__container-outer">
-        <Header />
-        <div className="header__desc-container">
-          <p className="header__desc-content">
-            Introducing Airbnb Luxe<br />
-            Extraordinary homes<br />
-            with five–star everything
-          </p>
-        </div>
-      </div>
-
-      <ListingIndexContainer />
+      <Route path="/" component={Header}/>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/listings" component={ListingIndexContainer} />
+      </Switch>
     </main>
   );
 }
---
+++ frontend/components/landing/landing.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Landing = props => {
+  return (
+    <div className="header__container-outer">
+      {/* <Header /> */}
+      <div className="header__desc-container">
+        <p className="header__desc-content">
+          Introducing Airbnb Luxe<br />
+          Extraordinary homes<br />
+          with five–star everything
+          </p>
+      </div>
+    </div>
+  );
+}
+
+
+export default Landing;
+
