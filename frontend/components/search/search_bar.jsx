d51d0b3bfaec6769c9c8c88adcb42f558756c308
Kenneth Choi <kennethichoi@gmail.com>
2019-07-07 16:15:40 -0700

    Implement mvp for autocomplete google search

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -1,35 +1,75 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 class SearchBar extends React.Component {
+  
   constructor(props) {
     super(props);
-
-    this.state = {};
+    
+    this.state = {
+      query: ""
+    };
+    
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleInput() {
-    return e => {
-      this.setState({
-        query: e.target.value
-      });
-    }
+  componentDidMount() {
+    const searchInput = document.querySelector(".header__search-input");
+    const autocomplete = new google.maps.places.Autocomplete(searchInput);
+    google.maps.event.addDomListener(window, "load", autocomplete);
+    let address;
+    autocomplete.addListener("place_changed", () => {
+      if (!autocomplete.getPlace().formatted_address) {
+        address = autocomplete.getPlace().name;
+        this.setState({ query: address });
+        this.handleSubmit();
+      } else {
+        address = autocomplete.getPlace().formatted_address;
+        this.setState({ query: address });
+        this.handleSubmit();
+      }
+    });
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  handleInput(e) {
+    this.setState({
+      query: e.currentTarget.value
+    });
   }
 
   handleSubmit(e) {
-    e.preventDefault();
-
+    // debugger
+    if (e) {
+      e.preventDefault();
+    }
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: this.state.query }, (res, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+        const lat = res[0].geometry.location.lat();
+        const lng = res[0].geometry.location.lng();
+        this.props.history.push(`/listings?lat=${lat}&lng=${lng}`);
+      } else {
+        this.props.history.push(`/listings?lat=37.7749&lng=122.4194`);
+      }
+    });
   }
 
   render() {
     return (
       <div className="header__search-container-inner">
         <div className="header__search-icon-container" />
-        <form className="header__search-form">
+        <form className="header__search-form" onSubmit={this.handleSubmit}>
           <input
             type="text"
             value={this.state.query}
             onChange={this.handleInput}
             className="header__search-input"
+            placeholder={`Try "San Francisco"`}
           />
           <input
             type="submit"
@@ -45,4 +85,4 @@ class SearchBar extends React.Component {
   }
 }
 
-export default SearchBar;
+export default withRouter(SearchBar);
