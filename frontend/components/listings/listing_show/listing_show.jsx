a8eca310eb03ea5802f00d52fe579d3280221a49
Kenneth Choi <kennethichoi@gmail.com>
2019-07-08 12:58:27 -0700

    Complete initial show overlay styling

--- frontend/components/listings/listing_show/listing_show.jsx
+++ frontend/components/listings/listing_show/listing_show.jsx
@@ -18,7 +18,7 @@ class ListingShow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true
+      loading: false
     }
 
     setTimeout(() => {
--- frontend/components/listings/listing_show/listing_show_info_overlay.jsx
+++ frontend/components/listings/listing_show/listing_show_info_overlay.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 
 class ListingShowInfoOverlay extends React.Component {
+  
   render() {
-    return (
-      <div className="listingshow__hero-container">
-        
-      </div>
+    let { listing } = this.props;
 
+    return (
+      <div className="listingshow__overlay-container">
+        <div className="listingshow__overlay-listing">
+          <div className="listingshow__overlay-logo-outer">
+            <div className="listingshow__overlay-logo" />
+          </div>
+          <div className="listingshow__overlay-listing-outer">
+            <h3 className="listingshow__overlay-listingtitle">
+              {listing.title}
+            </h3>
+            <div className="listingshow__overlay-listingstars">
+              {listing.average_rating} ({listing.num_reviews})
+            </div>
+          </div>
+        </div>
+        <div className="listingshow__overlay-availability-outer">
+          <div className="listingshow__overlay-pricing">
+            ${listing.price} <span>/ night</span>
+          </div>
+          <div className="listingshow__overlay-availability-btn-outer">
+            <button className="listingshow__overlay-availability-btn">
+              Check availability
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
